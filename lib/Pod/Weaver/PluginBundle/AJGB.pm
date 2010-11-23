90705eda4a9fb435337b4b5d812cdcf90da0bc47
Alex J. G. Burzyński <ajgb@cpan.org>
2010-11-23 20:37:01 +0000

    Pod::Weaver plugin bundle

---
+++ lib/Pod/Weaver/PluginBundle/AJGB.pm
@@ -0,0 +1,49 @@
+use strict;
+use warnings;
+package Pod::Weaver::PluginBundle::AJGB;
+#ABSTRACT: Pod::Weaver configuration for AJGB
+
+use Pod::Weaver::Config::Assembler;
+
+=head1 DESCRIPTION
+
+Pod::Weaver configuration used by L<Dist::Zilla::PluginBundle::AJGB>.
+
+=cut
+
+sub _exp { Pod::Weaver::Config::Assembler->expand_package($_[0]) }
+
+sub mvp_bundle_config {
+    return (
+        [ '@AJGB/CorePrep',     _exp('@CorePrep'),  {} ],
+
+        [ '@AJGB/Encoding',     _exp('-Encoding'),  {} ],
+
+        [ '@AJGB/Name',         _exp('Name'),       {} ],
+        [ '@AJGB/Version',      _exp('Version'),    {} ],
+
+
+        [ '@AJGB/Prelude',      _exp('Region'),     { region_name   => 'prelude'        } ],
+
+        [ '@AJGB/Synopsis',     _exp('Generic'),    { header        => 'SYNOPSIS'       } ],
+        [ '@AJGB/Description',  _exp('Generic'),    { header        => 'DESCRIPTION'    } ],
+        [ '@AJGB/Overview',     _exp('Generic'),    { header        => 'OVERVIEW'       } ],
+
+
+        [ 'ATTRIBUTES',         _exp('Collect'),    { command       => 'attr'           } ],
+        [ 'METHODS',            _exp('Collect'),    { command       => 'method'         } ],
+        [ 'FUNCTIONS',          _exp('Collect'),    { command       => 'func'           } ],
+        [ 'TYPES',              _exp('Collect'),    { command       => 'type'           } ],
+
+        [ '@AJGB/Leftovers',    _exp('Leftovers'),  {} ],
+
+        [ '@AJGB/Postlude',     _exp('Region'),     { region_name   => 'postlude'       } ],
+
+        [ '@AJGB/SeeAlso',      _exp('SeeAlso'),    {} ],
+
+        [ '@AJGB/Authors',      _exp('Authors'),    {} ],
+        [ '@AJGB/Legal',        _exp('Legal'),      {} ],
+    );
+}
+
+1;
