d6c410eaf91de8621d483312e7b76039fcb78321
Alex J. G. Burzyński <ajgb@cpan.org>
2012-05-08 22:07:07 +0100

    updated for 2012 (use git plugins and make sure that plugins are loaded in correct order)

--- lib/Dist/Zilla/PluginBundle/AJGB.pm
+++ lib/Dist/Zilla/PluginBundle/AJGB.pm
@@ -1,6 +1,8 @@
 use strict;
 use warnings;
+
 package Dist::Zilla::PluginBundle::AJGB;
+
 # ABSTRACT: Dist::Zilla plugins for AJGB
 
 use Moose;
@@ -15,36 +17,77 @@ with 'Dist::Zilla::Role::PluginBundle::Easy';
 
 This is the plugin bundle for AJGB. It's an equivalent to:
 
-    [@Basic]
 
+    [ExecDir]
+    [ShareDir]
+
+    [GatherDir]
+    exclude_filename = README
+    exclude_filename = dist.ini
+    exclude_filename = weaver.ini
+    [MetaYAML]
+    [MetaJSON]
+    [License]
+    [Manifest]
+    [Test::Compile]
+    [PodCoverageTests]
+    [PodSyntaxTests]
+    [EOLTests]
+    [NoTabsTests]
+    [KwaliteeTests]
+    [Test::Portability]
+    [Test::Synopsis]
+
+    [PruneCruft]
+    [ManifestSkip]
+
+    [ExtraTests]
+    [Authority]
+    authority = cpan:AJGB
+    do_metadata = 1
+    [PodWeaver]
+    config_plugin = @AJGB
+
+    [MetaConfig]
     [AutoMetaResources]
     bugtracker.rt = 1
     repository.github = user:ajgb
     homepage = http://search.cpan.org/dist/%{dist}
 
-    [MetaConfig]
+    [AutoPrereqs]
+    [MakeMaker]
     [ModuleBuild]
-    [PodCoverageTests]
-    [PodSyntaxTests]
-    [PkgVersion]
-    [AutoVersion]
 
-    [CheckChangesHasContent]
-    [CheckExtraTests]
-    [CompileTests]
-    [EOLTests]
-    [NoTabsTests]
     [InstallGuide]
-    [KwaliteeTests]
-    [PortabilityTests]
     [ReadmeFromPod]
 
-    [Authority]
-    authority = cpan:AJGB
-    do_metadata = 1
+    [Git::NextVersion]
+    first_version = 0.01
 
-    [PodWeaver]
-    config_plugin = @AJGB
+    [CopyFilesFromBuild]
+    copy = README
+
+    [Git::Check]
+    allow_dirty = Changes
+    allow_dirty = dist.ini
+    allow_dirty = README
+    [CheckChangesHasContent]
+    [CheckExtraTests]
+    [TestRelease]
+    [ConfirmRelease]
+
+    [UploadToCPAN]
+
+    [NextRelease]
+    time_zone = Europe/London
+    filename = Changes
+    format = %-6v %{yyyy-MM-dd HH:mm:ss}d
+    [Git::Tag]
+    filename = Changes
+    tag_format = %v
+    [Git::Commit]
+    time_zone = Europe/London
+    [Git::Push]
 
 =pod
 
@@ -61,53 +104,139 @@ sub configure {
 
     my $payload = $self->payload;
 
-    $self->add_bundle('Basic');
-
+    # Dirs
     $self->add_plugins(
         qw(
-            MetaConfig
-            ModuleBuild
-            PodCoverageTests
-            PodSyntaxTests
-            PkgVersion
-            MetaJSON
+          ExecDir
+          ShareDir
+        )
+    );
 
-            CheckChangesHasContent
-            CheckExtraTests
-            CompileTests
-            EOLTests
-            NoTabsTests
-            InstallGuide
-            KwaliteeTests
-            PortabilityTests
-            ReadmeFromPod
+    # FileGatherer
+    $self->add_plugins(
+        [
+            GatherDir =>
+              { exclude_filename => [ 'README', 'dist.ini', 'weaver.ini', ], }
+        ],
+        qw(
+          MetaYAML
+          MetaJSON
+          License
+          Manifest
+          Test::Compile
+          PodCoverageTests
+          PodSyntaxTests
+          EOLTests
+          NoTabsTests
+          KwaliteeTests
+          Test::Portability
+          Test::Synopsis
         ),
-        (
-            defined $payload->{version} && $payload->{vesion} =~ /\./ ? () :
-            [
-                AutoVersion => {
-                    major => $payload->{version} || 1
-                }
-            ]
+    );
+
+    # FilePruner
+    $self->add_plugins(
+        qw(
+          PruneCruft
+          ManifestSkip
+        ),
+    );
+
+    # FileMunger
+    $self->add_plugins(
+        qw(
+          ExtraTests
+        ),
+        [
+            Authority => {
+                authority   => 'cpan:AJGB',
+                do_metadata => 1,
+            }
+        ],
+        [ PodWeaver          => { config_plugin => '@AJGB', } ],
+    );
+
+    # MetaProvider
+    $self->add_plugins(
+        qw(
+          MetaConfig
         ),
         [
             AutoMetaResources => {
                 'repository.github' => 'user:ajgb',
-                'bugtracker.rt' => 1,
+                'bugtracker.rt'     => 1,
                 'homepage' => 'http://search.cpan.org/dist/%{dist}',
             },
         ],
+    );
+
+    # PrereqSource
+    $self->add_plugins(
+        qw(
+          AutoPrereqs
+          MakeMaker
+          ModuleBuild
+        ),
+    );
+
+    # PrereqSource / InstallTool
+    $self->add_plugins(
+        qw(
+          ReadmeFromPod
+          InstallGuide
+        ),
+    );
+
+    # VersionProvider
+    $self->add_plugins(
+        [ 'Git::NextVersion' => { first_version => '0.01', } ],
+    );
+
+    # AfterBuild
+    $self->add_plugins(
+        [ CopyFilesFromBuild => { copy          => 'README', } ],
+    );
+
+    # BeforeRelease
+    $self->add_plugins(
         [
-            Authority => {
-                authority => 'cpan:AJGB',
-                do_metadata => 1,
+            'Git::Check' =>
+              { allow_dirty => [ 'README', 'dist.ini', 'weaver.ini', ], }
+        ],
+        qw(
+          CheckChangesHasContent
+          CheckExtraTests
+          TestRelease
+          ConfirmRelease
+        ),
+    );
+
+    # Releaser
+    $self->add_plugins(
+        qw(
+          UploadToCPAN
+        )
+    );
+
+    # AfterRelease
+    $self->add_plugins(
+        [
+            NextRelease => {
+                time_zone => 'Europe/London',
+                filename  => 'Changes',
+                format    => '%-6v %{yyyy-MM-dd HH:mm:ss}d',
             }
         ],
         [
-            PodWeaver => {
-                config_plugin => '@AJGB',
+            'Git::Tag' => {
+                filename   => 'Changes',
+                tag_format => '%v',
             }
         ],
+        [ 'Git::Commit'      => { time_zone     => 'Europe/London', } ],
+        qw(
+          Git::Push
+        ),
     );
 }
 
